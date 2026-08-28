7adcb69d8d4a780eaf4bce38e588123868a67bb3
ithan2 <144854242+ithan2@users.noreply.github.com>
2026-08-28 11:52:10 -0300

    Praticamente finalizada, bugs na divisão e porcentagem.

--- java.js
+++ java.js
@@ -1,14 +1,66 @@
+var limparTela = false;
+
 function insert(num)
 {
+    if (limparTela == true)
+    {
+        document.getElementById('resu').innerHTML = "";
+        limparTela = false;
+    }
+
     var numero = document.getElementById('resu').innerHTML;
     document.getElementById('resu').innerHTML = numero + num;
+
+    atualizarPrevia();
 }
+
 function clean()
 {
     document.getElementById('resu').innerHTML = "";
+
 }
+
 function back()
 {
-    var resultado = document.getElementById('Resu').innerHTML;
-    document.getElementById('resu').innerHTML = resultado.substring(0,resu -1)
+    var resu = document.getElementById('resu').innerHTML;
+    document.getElementById('resu').innerHTML = resu.substring(0, resu.length - 1);
+
+    atualizarPrevia();
+
+}
+
+function calcular()
+{
+    var resultado = document.getElementById('resu').innerHTML;
+
+    if (resultado)
+    {
+        document.getElementById('resu').innerHTML = eval(resultado);
+        limparTela = true;
+    }
+    else
+    {
+        document.getElementById('resu').innerHTML = "Nada...";
+    }
+}
+
+function atualizarPrevia()
+{
+    var conta = document.getElementById('resu').innerHTML;
+
+    try
+    {
+        if (conta != "")
+        {
+            document.getElementById('previa').innerHTML = eval(conta);
+        }
+        else
+        {
+            document.getElementById('previa').innerHTML = "";
+        }
+    }
+    catch
+    {
+        document.getElementById('previa').innerHTML = "";
+    }
 }
